f4694777b3167964870dc4fedf19360b5f48e447
PoutsBF <stephane.lepoutere@free.fr>
2020-11-07 21:39:12 +0100

    maj commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 /******************************************************************************
     WebLed                                         Stéphane Lepoutère (c) 2020
 
-    Observe l'arrivée de changement d'état sur les BP (interruption)
-    Gère le débounce
-    Détecte les appuis courts, longs, doubles clics et l'état instantané
-    2020-11-07 : modification de la philosophie du double clic :
-        un double clic est considéré comme un clic qui arrive dans un délai 
-        court après le précédent. Le premier clic est sensé avoir été pris 
-        en compte.
+    leds néopixel contôlées par BP ou page web à l'aide de websockets
 */
 
 #include <ArduinoOTA.h>
